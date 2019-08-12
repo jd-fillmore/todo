d38605abf6633c08111b06733b948315e598224a
Fillmore <jfillmore@ehs.com>
2019-08-12 16:56:01 -0400

    remove validation, row styles

--- app.js
+++ app.js
@@ -5,23 +5,7 @@
 const searchInput = document.getElementById("input");
 const button = document.getElementById("button");
 const ulList = document.querySelector(".to-do-list");
-
-// Validation
-
-// If there's no list items on the page, tell the user to add some
-
 const liItem = document.querySelector(".to-do-item");
-
-if (!liItem) {
-  // Create message
-  const message = document.createElement("p");
-  // Add class
-  message.classList.add("message");
-  // Add text
-  message.innerHTML = "Congrats! No to do items. Why don't you add some?";
-  // Add to dom
-  ulList.parentNode.appendChild(message);
-}
 
 ///// Add to do
 
@@ -47,47 +31,43 @@ function addToDo() {
   if (searchValue) {
     // Create an li element
     const li = document.createElement("li");
+
     // Add class to li
     li.classList.add("to-do-item");
+
     // Add the content of what the user inputted
     li.innerHTML = searchValue;
+
     // Add to dom
     ulList.appendChild(li);
+
     // Clear input value
     searchInput.value = "";
-
-    ////// Add the trash can (delete) icon to the dom
 
     // Create the trash can and check element
     const trashCan = document.createElement("i");
     const checkMark = document.createElement("i");
+
     // Add classes
     trashCan.classList.add("fa", "fa-trash");
     checkMark.classList.add("fa", "fa-check-circle");
+
+    // Add a div to wrap around both the trash can and the check mark, so we can flex them on the right hand side
+    const divWrap = document.createElement("div");
+
+    // Add class
+    divWrap.classList.add("right");
+
     // Add to dom
-    li.appendChild(checkMark);
-    li.appendChild(trashCan);
+    li.appendChild(divWrap);
+    divWrap.appendChild(checkMark);
+    divWrap.appendChild(trashCan);
 
     // Add click event for completing item
     checkMark.addEventListener("click", completeToDo);
 
     // Add click event for removing item
     trashCan.addEventListener("click", deleteToDo);
-
-    // If there's list items, remove message text
-    const message = document.querySelector(".message");
-    if (li) {
-      message.remove();
-    } else {
-      // Create message
-      const message = document.createElement("p");
-      // Add class
-      message.classList.add("message");
-      // Add text
-      message.innerHTML = "Congrats! No to do items. Why don't you add some?";
-      // Add to dom
-      ulList.parentNode.appendChild(message);
-    }
   } else {
     alert("Please enter something to do.");
   }
@@ -96,14 +76,16 @@ function addToDo() {
 ///// Complete to do
 
 function completeToDo() {
-  const li = this.parentNode;
+  const li = this.parentNode.parentNode;
 
-  li.classList.add("line-through");
+  setTimeout(function() {
+    li.classList.add("line-through");
+  }, 500);
 }
 
-// Delete to do
+///// Delete to do
 
 function deleteToDo() {
-  const li = this.parentNode;
+  const li = this.parentNode.parentNode;
   li.remove();
 }
